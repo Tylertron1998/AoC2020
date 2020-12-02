56ce651a7e7bf4d5a9d8730553bc24cebd309e92
Tyler Davis <tgfdavis98@gmail.com>
2020-12-02 08:32:52 +0000

    Add Day One Solution

--- AdventOfCode/DayOne/Program.cs
+++ AdventOfCode/DayOne/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DayOne
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(FileInfo file = null)
         {
-            Console.WriteLine("Hello World!");
+            file ??= new FileInfo("input.txt");
+            
+            var path = file.FullName;
+            var firstList = await File.ReadAllLinesAsync(path);
+            var secondList = firstList.Clone() as string[];
+
+            var lhs = 0;
+            var rhs = 0;
+
+            foreach (var x in firstList)
+            {
+                var firstNumericValue = int.Parse(x);
+
+                foreach (var y in secondList)
+                {
+                    var secondNumericValue = int.Parse(y);
+
+                    if (firstNumericValue + secondNumericValue == 2020)
+                    {
+                        lhs = firstNumericValue;
+                        rhs = secondNumericValue;
+                    }
+                }
+            }
+
+
+            Console.WriteLine(lhs * rhs);
         }
     }
 }
