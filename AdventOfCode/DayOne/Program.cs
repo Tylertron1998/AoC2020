59344964c2ca08181a0ea6496749fa11f5ad4ff5
Tyler Davis <tgfdavis98@gmail.com>
2020-12-02 09:10:57 +0000

    Add day two, Directory.Build.props, rename namespace in day one

--- AdventOfCode/DayOne/Program.cs
+++ AdventOfCode/DayOne/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using DayOne.Solvers;
 
-namespace DayOnePartOne
+namespace DayOne
 {
     public static class Program
     {
