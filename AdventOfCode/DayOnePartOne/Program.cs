77b376fb160d696a0272bdbeac67dc1cf11f984a
Tyler Davis <tgfdavis98@gmail.com>
2020-12-02 08:46:22 +0000

    Cleanup

---
+++ AdventOfCode/DayOnePartOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DayOnePartOne
+{
+    public static class Program
+    {
+        public static async Task Main(FileInfo file = null)
+        {
+            file ??= new FileInfo("input.txt");
+            
+            var path = file.FullName;
+            var lines = await File.ReadAllLinesAsync(path);
+            var values = lines.Select(int.Parse);
+
+
+            foreach (var firstNumericValue in values)
+            {
+                foreach (var secondNumericValue in values)
+                {
+                    if (firstNumericValue + secondNumericValue == 2020)
+                    {
+                        Console.WriteLine(firstNumericValue * secondNumericValue);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
