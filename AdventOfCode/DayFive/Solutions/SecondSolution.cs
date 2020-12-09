5462dc1ccfd3a60039339c7f62233d35dda35a8e
Tyler Davis <tgfdavis98@gmail.com>
2020-12-09 09:57:32 +0000

    Finish up day five

--- AdventOfCode/DayFive/Solutions/SecondSolution.cs
+++ AdventOfCode/DayFive/Solutions/SecondSolution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Shared.Solutions;
 using DayFive.Parsing;
@@ -11,28 +12,28 @@ namespace DayFive.Solutions
 		{
 			var parser = new TicketParser();
 
-			var ids = inputs.Select(parser.Parse).ToArray();
+			var parsed = inputs.Select(parser.Parse).ToArray();
 			
 
 			var mySeat = -1;
 
-			var seats = new int[128, 8];
-
-			foreach (var (row, column, id) in ids)
+			var seats = new HashSet<int>(parsed.Select(x =>
 			{
-				if (row == 0 || row == 127) seats[row, column] = 1;
-				seats[row, column] = id;
-			}
+				var (_, _, id) = x;
+				return id;
+			}));
 
-			for (var x = 0; x < 128; x++)
+			for (var x = 0; x < ((128 * 8) + 8); x++)
 			{
-				for (var y = 0; y < 8; y++)
+				if (seats.TryGetValue(x, out _))
 				{
-					var seat = seats[x, y];
-					if (seat == 0) mySeat = (x * 8) + y;
+					if (seats.TryGetValue(x + 2, out _))
+					{
+						if (seats.TryGetValue(x + 1, out _)) continue;
+						mySeat = x + 1;
+					}
 				}
 			}
-				
 
 			if (mySeat == -1) throw new ArgumentException();
 
