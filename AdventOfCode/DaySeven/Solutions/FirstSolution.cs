1459fed3e3c4a44f7ecd46508106d324e3e3cef2
Tyler Davis <tgfdavis98@gmail.com>
2020-12-10 13:44:19 +0000

    commit before changing workspace

---
+++ AdventOfCode/DaySeven/Solutions/FirstSolution.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode.Shared.Solutions;
+
+namespace DaySeven.Solutions
+{
+	public class FirstSolution : ISolution
+	{
+		public string Solve(string[] inputs)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ AdventOfCode/DaySeven/Solutions/SecondSolution.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode.Shared.Solutions;
+
+namespace DaySeven.Solutions
+{
+	public class SecondSolution : ISolution
+	{
+		public string Solve(string[] inputs)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
